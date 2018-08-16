d4c72f5e8a6705649e5182a7fac62d9034224112
lixiaode222 <1803118517@qq.com>
2018-08-16 02:40:29 +0000

    Add password reset & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -93,13 +93,11 @@ public function update(User $user, Request $request)
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '1803118517@qq.com';
-        $name = 'lixiaode';
         $to = $user->email;
-        $subject = "感谢注册 TTalk 应用！请确认你的邮箱。";
+        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
